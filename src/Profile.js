3181862f227fe44aaccb205be29337a14c7cc6e8
Kevin Desktop <kevinkant@knights.ucf.edu>
2022-03-31 18:38:07 -0400

    Update

--- src/Profile.js
+++ src/Profile.js
@@ -1,3 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import React, { useState } from 'react';
+
+
+// Profile Screen
+function ProfileScreen(){
+    return(
+        <View
+            <ProfilePicture />
+    )
+}
+
+function ProfilePicture(){
+    return(
+        <View
+        
+    )
+}
