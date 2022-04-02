247cc56fe82b4cfcd5358e1cc443f5adfc588f7a
Zackary Kiener <64226837+ZackFred07@users.noreply.github.com>
2022-04-02 00:00:17 -0400

    Update CalorieTracker.js

--- src/CalorieTracker.js
+++ src/CalorieTracker.js
@@ -1,3 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Dimensions } from 'react-native';
 import React, { useState } from 'react';
+
+//Variables to get the width and height of the screen (That way it works for any screensize(?))
+const screenWidth = Dimensions.get("window").width;
+const screenHeight = Dimensions.get("window").height;
+
+
+
+const tableConfig = {
+    
+}
+
+
+
+
+
+
+
+
+//CalorieTracker Screen
+export default function App() {
+    return (
+        <View style={styles.container}>
+        <Title />
+        <Dropdown />
+        <SearchButton />
+        <StatusBar style="auto" showHideTransition={'fade'} />
+        <NavBar />
+    </View>
+    );
+}
+
+//Title
+
+
+//Dropdown
+const Dropdown = () => {
+    return (
+        <View style={styles.box}>
+            <h1>Custom Select/dropdown</h1>
+            <DropDownContainer>
+              <DropDownHeader>Enter Food</DropDownHeader>
+              <DropDownListContainer>
+                <DropDownList>
+                  <ListItem>Mangoes</ListItem>
+                  <ListItem>Apples</ListItem>
+                  <ListItem>Oranges</ListItem>
+            </DropDownList>
+          </DropDownListContainer>
+        </DropDownContainer>
+      </View>
+    );
+}
+
+//SearchButton
+
+
+
+
+
+//NavBar stuff I stole
+const NavBar = () => {
+    return (
+      <View style={styles.navBar}>
+  
+      </View>
+    )
+  }
