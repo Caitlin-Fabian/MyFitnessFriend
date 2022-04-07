6b0857f458789f0f97dfb4d9ecc0b33aec3e5520
Kevin Desktop <kevinkant@knights.ucf.edu>
2022-04-07 12:35:58 -0400

    Merge branch 'master' of https://github.com/Caitlin-Fabian/MyFitnessFriend

--- App.js
+++ App.js
@@ -7,8 +7,8 @@ import CalorieTracker from './src/CalorieTracker'
 import ExerciseRoutines from './src/ExcerciseRoutines';
 import ExerciseIntervals from './src/ExcerciseInterval';
 import RegistrationScreen from './src/RegistrationScreen';
-import Login from './src/LoginScreen';
 import NavBar from './src/NavBar';
+import LoginScreen from './src/LoginScreen';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
@@ -19,9 +19,7 @@ export default function App() {
 
   const [user, setUser] = useState(null);
 
-  function getUser() {
 
-  }
 
   useEffect(() => {
     const auth = getAuth();
