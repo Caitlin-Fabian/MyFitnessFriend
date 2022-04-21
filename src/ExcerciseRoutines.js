3b389b886a641388f3b68c10100a2bfe19ecf017
Nader Fares <naderfares@Naders-MacBook-Pro.local>
2022-04-21 18:03:04 -0400

    add edit functionality to individual workouts and  clear text boxes after use

--- src/ExcerciseRoutines.js
+++ src/ExcerciseRoutines.js
@@ -14,12 +14,13 @@ function ExerciseRoutines() {
         setRoutines((prevRoutines) => {
             const tempRoutineIndex = prevRoutines.findIndex(routine => routineName === routine.name)
             prevRoutines[tempRoutineIndex].workouts = prevRoutines[tempRoutineIndex].workouts.filter(workout => workout.name !== name)
-            return [ ...prevRoutines ]
+            setEditWorkoutModal({open: false, routineName: null, workoutName: null})
+            return [...prevRoutines]
         })
     }
 
-    useEffect( async () => {
-        if(!userInfo) {
+    useEffect(async () => {
+        if (!userInfo) {
             const auth = getAuth();
             const userData = await User.getUserInfo(auth.currentUser.uid); //Pulls user info from the firebase
             setUserInfo(userData);
@@ -49,28 +50,29 @@ function ExerciseRoutines() {
 
     const submitAddWorkoutHandler = async (name, reps, sets, routineName) => {
         // checks if fields are empty
-        if (!name || !reps || !sets)
-        {
+        if (!name || !reps || !sets) {
             Alert.alert('Alert', 'Make sure there are no empty fields', [
-                {text: 'Ok', onPress: () => console.log('alert close')}
+                { text: 'Ok', onPress: () => console.log('alert close') }
             ])
         }
-        else 
-        setRoutines((prevRoutines) => {
-            const tempRoutines = [
-                ...prevRoutines,
-            ]
-            tempRoutines.find(routine => routine.name === routineName)?.workouts.push({ name, reps, sets })
-            User.addRoutines(tempRoutines, userInfo.uid);
-            closeAddWorkoutModal()
-            return tempRoutines;
-        })
+        else
+            setRoutines((prevRoutines) => {
+                const tempRoutines = [
+                    ...prevRoutines,
+                ]
+                tempRoutines.find(routine => routine.name === routineName)?.workouts.push({ name, reps, sets })
+                User.addRoutines(tempRoutines, userInfo.uid);
+                changeRepHandler('')
+                changeSetHandler('')
+                changeNameHandler('')
+                return tempRoutines;
+            })
     }
 
     const submitAddRoutineHandler = (routineName) => {
         if (!routineName)
             Alert.alert('Alert', 'Make sure there are no empty fields', [
-                {text: 'Ok', onPress: () => console.log('alert close')}
+                { text: 'Ok', onPress: () => console.log('alert close') }
             ])
         else
             setRoutines((prevRoutines) => {
@@ -87,9 +89,13 @@ function ExerciseRoutines() {
     ]);
 
     const [addWorkoutModal, setAddWorkoutModal] = useState(
-        { open: false, routineName: null}
+        { open: false, routineName: null }
     );
     const [addRoutineModal, setAddRoutineModal] = useState(false);
+
+    const [editWorkoutModal, setEditWorkoutModal] = useState(
+        { open: false, routineName: null, workoutName: null }
+    );
 
     const closeAddWorkoutModal = () => {
         setNameText('')
@@ -97,14 +103,14 @@ function ExerciseRoutines() {
         setRepText('')
         setAddWorkoutModal({ open: false, routineName: null });
     }
-
+    
     const closeAddRoutineModal = () => {
         setRoutineNameText('')
         setAddRoutineModal(false)
     }
     const addWorkout = (nameText, repText, setText) => {
         submitAddWorkoutHandler(nameText, repText, setText, addWorkoutModal.routineName);
-        
+
     }
 
     const deleteRoutineHelper = (routineName) => {
@@ -112,7 +118,7 @@ function ExerciseRoutines() {
             const tempRoutine = prevRoutines.filter(routine => routineName !== routine.name)
             prevRoutines = tempRoutine
             console.log(prevRoutines)
-            return [ ...prevRoutines ]
+            return [...prevRoutines]
         })
         User.removeRoutines(routineName, userInfo.uid);
     }
@@ -125,99 +131,132 @@ function ExerciseRoutines() {
     const addRoutine = (routineName) => {
         submitAddRoutineHandler(routineName)
     }
-    
+
     return (
-        <View style={{flex: 1}}>
-            <ScrollView style={{backgroundColor: '#96BDC6', flex: 1}}>
+        <View style={{ flex: 1 }}>
+            <ScrollView style={{ backgroundColor: '#96BDC6', flex: 1 }}>
 
-            <View style={styles.container}>
-                <View style={styles.buttonContainer}>
-                    <Button title='add routine' onPress={() => setAddRoutineModal(true)} />
-                </View>
-
-                <View style={styles.titleContainer}>
-                    <Text style={styles.title}>Routines</Text>
-                </View>
-
-                <Modal visible={addRoutineModal}>
-                    <View style={styles.container}>
-                        <Text style={{fontSize: 36, padding: 10}}>Add Routine</Text>
-                        <TextInput
-                        style={styles.inputContainer}
-                        placeholder='routine name'
-                        onChangeText={changeRoutineNameHandler}
-                        />
-                        <Button title='exit' onPress={() => setAddRoutineModal(false)}/>
-                        <Button title='add routine' onPress={() => addRoutine(routineNameText)}/>
+                <View style={styles.container}>
+                    <View style={styles.buttonContainer}>
+                        <Button title='add routine' onPress={() => setAddRoutineModal(true)} />
                     </View>
-                </Modal>
 
-                <Modal visible={addWorkoutModal.open}>
-                    <View style={styles.container}>
-                        <View style={styles.titleContainer}>
-                            <Text style={styles.title}>{addWorkoutModal.routineName}</Text>
-                        </View>
-                        <Text style={{marginTop: 50, fontSize: 28, textAlign: 'center'}}>Add Workout</Text>
-                        <View style={{padding: 30, justifyContent: 'center', alignItems: 'center'}}>
-                            <TextInput
-                            style={styles.inputContainer}
-                            placeholder='workout name'
-                            onChangeText={changeNameHandler}
-                            />
-                            <TextInput
-                            style={styles.inputContainer}
-                            placeholder='rep number'
-                            keyBoardType='numeric'
-                            onChangeText={changeRepHandler}
-                            />
-                            <TextInput
-                            style={styles.inputContainer}
-                            placeholder='set number'
-                            keyBoardType='numeric'
-                            onChangeText={changeSetHandler}
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>Routines</Text>
+                    </View>
 
+
+
+                    <Modal visible={addRoutineModal}>
+                        <View style={styles.container}>
+                            <Text style={{ fontSize: 36, padding: 10 }}>Add Routine</Text>
+                            <TextInput
+                                style={styles.inputContainer}
+                                placeholder='routine name'
+                                onChangeText={changeRoutineNameHandler}
                             />
-                            <Button title='exit' onPress={() => closeAddWorkoutModal()}/>
-                            <Button title='delete routine' onPress={() => deleteRoutine(addWorkoutModal.routineName)} />
-                            <Button title='add workout' onPress={() => addWorkout( nameText, repText, setText )}/>
+                            <Button title='exit' onPress={() => setAddRoutineModal(false)} />
+                            <Button title='add routine' onPress={() => addRoutine(routineNameText)} />
                         </View>
-                        <View style={{height: 200}}>
-                            <ScrollView style={{backgroundColor: '#E8CCBF', borderRadius: 5, borderWidth: 2, borderColor: '#000000', width: 375, marginHorizontal: 20}}>
-                                    { (routines).find(routine => routine.name === addWorkoutModal.routineName)?.workouts.map((workout) => (
-                                            <View style={{left: 3}} key={workout.name}>
-                                                <TouchableOpacity onPress={() => workoutPressHandler(workout.name, addWorkoutModal.routineName)}>
-                                                    <Text style={{fontSize: 16, fontWeight: 'bold', flex: 1, marginTop: 24}}>{workout.name}</Text>
-                                                    <Text style={{fontSize: 12}}>{workout.sets}x{workout.reps}</Text>
-                                                </TouchableOpacity>
+                    </Modal>
+
+                    <Modal visible={addWorkoutModal.open}>
+                        <View style={styles.container}>
+
+                            <Modal visible={editWorkoutModal.open}>
+                                <View style={styles.container}>
+                                    <Text style={{ marginTop: 50, fontSize: 28, textAlign: 'center' }}>Edit Workout</Text>
+                                    <View style={{ padding: 30, justifyContent: 'center', alignItems: 'center' }}>
+                                        <TextInput
+                                            style={styles.inputContainer}
+                                            placeholder='workout name'
+                                            onChangeText={changeNameHandler}
+                                        />
+                                        <TextInput
+                                            style={styles.inputContainer}
+                                            placeholder='rep number'
+                                            keyBoardType='numeric'
+                                            onChangeText={changeRepHandler}
+                                        />
+                                        <TextInput
+                                            style={styles.inputContainer}
+                                            placeholder='set number'
+                                            keyBoardType='numeric'
+                                            onChangeText={changeSetHandler}
+                                        />
+                                        <Button title='exit' onPress={() => setEditWorkoutModal({ open: false, routineName: null, workoutName: null })} />
+                                        <Button title='delete workout' onPress={() =>  workoutPressHandler(editWorkoutModal.workoutName, editWorkoutModal.routineName)} />
+                                        <Button title='edit' onPress={() => addWorkout(nameText, repText, setText)} />
+                                    </View>
+                                </View>
+                            </Modal>
+
+                            <View style={styles.titleContainer}>
+                                <Text style={styles.title}>{addWorkoutModal.routineName}</Text>
+                            </View>
+                            <Text style={{ marginTop: 50, fontSize: 28, textAlign: 'center' }}>Add Workout</Text>
+                            <View style={{ padding: 30, justifyContent: 'center', alignItems: 'center' }}>
+                                <TextInput
+                                    style={styles.inputContainer}
+                                    placeholder='workout name'
+                                    onChangeText={changeNameHandler}
+                                    value={nameText}
+                                />
+                                <TextInput
+                                    style={styles.inputContainer}
+                                    placeholder='rep number'
+                                    keyBoardType='numeric'
+                                    onChangeText={changeRepHandler}
+                                    value={repText}
+                                />
+                                <TextInput
+                                    style={styles.inputContainer}
+                                    placeholder='set number'
+                                    keyBoardType='numeric'
+                                    onChangeText={changeSetHandler}
+                                    value={setText}
+                                />
+                                <Button title='exit' onPress={() => closeAddWorkoutModal()} />
+                                <Button title='delete routine' onPress={() => deleteRoutine(addWorkoutModal.routineName)} />
+                                <Button title='add workout' onPress={() => addWorkout(nameText, repText, setText)} />
+                            </View>
+                            <View style={{ height: 200 }}>
+                                <ScrollView style={{ backgroundColor: '#E8CCBF', borderRadius: 5, borderWidth: 2, borderColor: '#000000', width: 375, marginHorizontal: 20 }}>
+                                    {(routines).find(routine => routine.name === addWorkoutModal.routineName)?.workouts.map((workout) => (
+                                        <View style={{ left: 3, margin: 2 }} key={workout.name}>
+                                            <TouchableOpacity onPress={() => setEditWorkoutModal({ open: true, routineName: addWorkoutModal.routineName, workoutName: workout.name })}>
+                                                <Text style={{ fontSize: 16, fontWeight: 'bold', flex: 1, marginTop: 24 }}>{workout.name}</Text>
+                                                <Text style={{ fontSize: 12 }}>{workout.sets}x{workout.reps}</Text>
+                                            </TouchableOpacity>
+                                        </View>
+                                    ))}
+                                </ScrollView>
+                            </View>
+                        </View>
+                    </Modal>
+
+
+
+                    <View style={{ height: 200 }}>
+                        {routines.map(routine =>
+                            <>
+                                <View style={{ paddingTop: 10 }}>
+                                    <Text style={styles.routineName}>{routine.name}</Text>
+                                </View>
+                                <TouchableOpacity onPress={() => setAddWorkoutModal({ open: true, routineName: routine.name })}>
+                                    <ScrollView style={{ backgroundColor: '#E8CCBF', borderRadius: 10, borderWidth: 2, borderColor: '#000000', width: 375, flex: 1, paddingHorizontal: 20 }}>
+                                        {routine.workouts.map((item) => (
+                                            <View style={{ left: 3 }} key={item.name}>
+                                                <Text style={{ fontSize: 16, fontWeight: 'bold', flex: 1, marginTop: 24 }}>{item.name}</Text>
+                                                <Text style={{ fontSize: 12 }}>{item.sets}x{item.reps}</Text>
                                             </View>
                                         ))}
-                                </ScrollView>
-                        </View>
+                                    </ScrollView>
+                                </TouchableOpacity>
+                            </>
+                        )}
                     </View>
-                </Modal>
-
-
-
-                <View style={{height: 200}}>
-                    {routines.map(routine =>
-                    <>
-                    <View style={{paddingTop: 10}}>
-                    <Text style={styles.routineName}>{routine.name}</Text>
-                    </View>
-                    <TouchableOpacity onPress={() => setAddWorkoutModal({ open: true, routineName: routine.name})}>
-                        <ScrollView style={{backgroundColor: '#E8CCBF', borderRadius: 10,borderWidth: 2, borderColor: '#000000', width: 375, flex: 1, paddingHorizontal: 20}}>
-                            { routine.workouts.map((item) => (
-                                    <View style={{left: 3}} key={item.name}>
-                                        <Text style={{fontSize: 16, fontWeight: 'bold', flex: 1, marginTop: 24}}>{item.name}</Text>
-                                        <Text style={{fontSize: 12}}>{item.sets}x{item.reps}</Text>
-                                    </View>
-                                ))}
-                        </ScrollView>
-                    </TouchableOpacity>
-                    </>
-                    )}
                 </View>
-            </View>
             </ScrollView>
         </View>
     )
